bb27315be922d5a3b8e37efa8e9387b3ef47f18f
AdrianCor <cornejoadrian93@gmail.com>
2025-01-19 22:49:43 -0500

    Imágenes

--- DCCatAPI/Models/Cat.cs
+++ DCCatAPI/Models/Cat.cs
@@ -12,5 +12,6 @@ namespace DCCatAPI.Models
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Origin { get; set; } = string.Empty;
+        public CatImage Image { get; set; } = new();
     }
 }
---
+++ DCCatAPI/Models/CatImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCCatAPI.Models
+{
+    public class CatImage
+    {
+        public string Url { get; set; } = string.Empty;
+    }
+}
